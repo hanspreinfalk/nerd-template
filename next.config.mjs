d13eec888909e29921177c4a34961a2501ba16f4
Hans Preinfalk <hans.preinfalk.davila@gmail.com>
2026-05-02 13:28:27 -0700

    allowed dev origins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  allowedDevOrigins: [
+    'e2b.app',
+    '*.e2b.app',
+    'e2b.dev',
+    '*.e2b.dev',
+  ],
+}
 
 export default nextConfig
